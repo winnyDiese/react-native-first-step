745b7b0366bc26730b8af461faf7e7a2df83d7c9
winnyDiese <winnydiese@gmail.com>
2023-10-24 22:27:22 +0200

    First step in react native

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import About from './components/About';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Text>My name is Maradona :) </Text>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    <About/>
   );
 }
 
---
+++ components/About.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { View } from 'react-native'
+
+export default function About (){
+    return(
+        <View>
+            <Text>Apropos de moi</Text>
+            <Text>Lorem ipsu uyb bubuybiubi bbujyvtjytv iuyvytv iytvuytv iytvuytv iytvuy </Text>
+        </View>
+    )
+}
